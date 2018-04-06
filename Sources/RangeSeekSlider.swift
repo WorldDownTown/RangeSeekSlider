6c65e427706444e9c99aac0de40f15d369db0928
Ozgur Odabasi <ozgurodabasi@gmail.com>
2018-04-06 15:38:13 +0300

    Swift 4.0 compatibility fixes

--- Sources/RangeSeekSlider.swift
+++ Sources/RangeSeekSlider.swift
@@ -489,11 +489,11 @@ import UIKit
         }
 
         if let nsstring = minLabel.string as? NSString {
-            minLabelTextSize = nsstring.size(attributes: [NSFontAttributeName: minLabelFont])
+            minLabelTextSize = nsstring.size(withAttributes: [kCTFontAttributeName as NSAttributedStringKey: minLabelFont])
         }
 
         if let nsstring = maxLabel.string as? NSString {
-            maxLabelTextSize = nsstring.size(attributes: [NSFontAttributeName: maxLabelFont])
+            maxLabelTextSize = nsstring.size(withAttributes: [kCTFontAttributeName as NSAttributedStringKey: maxLabelFont])
         }
     }
 
