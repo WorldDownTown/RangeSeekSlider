a8aceb8f9969cec57caa91e46b21b2738a0fac06
Sereivoan Yong <sereivoanyong@gmail.com>
2020-01-20 11:15:49 +0700

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+  name: "RangeSeekSlider",
+  platforms: [.iOS(.v9)],
+  products: [
+    .library(name: "RangeSeekSlider", targets: ["RangeSeekSlider"])
+  ],
+  targets: [
+    .target(name: "RangeSeekSlider", path: "Sources")
+  ],
+  swiftLanguageVersions: [.v5]
 )
