40cf6281376f99b6949ae320af6b9307f1bfbb55
Wolvorin <aalokparikh@yahoo.com>
2020-10-27 14:45:40 +0530

    Update Package.swift
    Update Package.swift for adding support for SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    products: [
+        .library(name: "RangeSeekSlider", targets: ["RangeSeekSlider"])
+    ],
+    targets: [
+        .target(
+            name: "RangeSeekSlider",
+            dependencies: [],
+            path: "../",
+        )
+    ]
 )
