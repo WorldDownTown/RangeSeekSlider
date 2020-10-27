d55aa847068db9a40196ae0a97eb9f71d5ffa0a8
Wolvorin <aalokparikh@yahoo.com>
2020-10-27 15:38:41 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,8 +9,7 @@ let package = Package(
     targets: [
         .target(
             name: "RangeSeekSlider",
-            dependencies: [],
-            path: "../Sources"
+            dependencies: []
         )
     ]
 )
