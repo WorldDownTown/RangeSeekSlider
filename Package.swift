8864990723288ac29bf787b259c18b949c40388a
Yannick Heinrich <yannick.heinrich@gmail.com>
2023-09-07 14:12:48 +0200

    Update tool-version
    Use Xcode 14.3.1 to set the current tools version

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version: 5.8
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]),
+    ],
+    dependencies: [],
+    targets:[
+        .target(
+            name: "RangeSeekSlider",
+            path: "Sources"
+        )
+    ]
 )
